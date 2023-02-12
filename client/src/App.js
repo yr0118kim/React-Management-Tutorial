175de6cce9549196f529c4f387e22a390af96a7c
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-02-13 00:28:31 +0900

    Add: Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,77 @@
 import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
-import { Table ,TableHead,TableBody,TableRow ,TableCell,withStylesm,Paper} from '@mui/material';
+import { Table, TableHead, TableBody, TableRow, TableCell, withStylesm, Paper,CircularProgress} from '@mui/material';
 
 const styles = {
   root: {
-  width: "100%",
-  overflowX: "auto"
+    width: "100%",
+    overflowX: "auto"
   },
   table: {
-  minWidth: 1080
+    minWidth: 1080
+  },
+};
+
+class App extends Component {
+  state = {
+    customers: "",
+    completed:0
   }
-  };
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '김예림',
-  'birthday': '060118',
-  'gender': '여자',
-  'job': 'student'
-}, {
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '김예봄',
-  'birthday': '060118',
-  'gender': '여자',
-  'job': 'student'
-}, {
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '김주완',
-  'birthday': '060118',
-  'gender': '여자',
-  'job': 'student'
-}]
+  componentDidMount() {
+    this.timer = setInterval(this.progress,20);
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
 
-function App(props) {
-  const {classes} = props;
-  return (
-    <Paper sx={{overflowX: "auto"}}>
-      <Table stickyHeader sx={{maxWidth: '1080px', minWidth: '1080px'}}>
-        <TableHead>
+  callApi = async () => {
+    const response = await fetch("/api/customers", {
+      headers: {
+        Accept: "application / json",
+      },
+      method: "GET",
+    })
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed:completed >=100 ? 0 : completed+1});
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper sx={{ overflowX: "auto" }}>
+        <Table stickyHeader sx={{ maxWidth: '1080px', minWidth: '1080px' }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+        <TableBody>
+          {this.state.customers ? this.state.customers.map(c => {
+            return (<Customer key={c.id} id={c.id} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} image={c.image} />);
+          }) : 
           <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-      </Table>
-      {
-        customers.map(c => {
-          return (
-            <Customer
-            key={c.id}
-              id={c.id}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-              image={c.image}
-            />
-          );
-          })
-        }
-    </Paper>
-  )
-      }
+            <TableCell colSpan="6" align='center'> 
+              <CircularProgress variant="determinate" value={this.state.completed}/>
+            </TableCell>
+            </TableRow>}
+        </TableBody>
+      </Paper>
+    )
+  }
+}
 
 export default App;
--- server.js
+++ server.js
@@ -7,10 +7,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message:'Hello Express!'});
+app.get('/api/customers',(req,res)=>{
+    res.send([{
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '김예림',
+        'birthday': '060118',
+        'gender': '여자',
+        'job': 'student'
+      }, {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '김예봄',
+        'birthday': '060118',
+        'gender': '여자',
+        'job': 'student'
+      }, {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '김주완',
+        'birthday': '060118',
+        'gender': '남자',
+        'job': 'student'
+      }]
+      );
 
 })
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`);
-})
+app.listen(port,()=>
+    console.log(`Listening on port ${port}`)
+);
