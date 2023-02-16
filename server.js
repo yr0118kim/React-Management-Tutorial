6db1d2a6d36da670335b33ff9a7980dc5e1b6168
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-02-17 01:44:50 +0900

    Create Customer DB Table & Node.js settings

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+const conn = 
+{
+    "host": "localhost",
+    "port": "3306",
+    "user": "user2",
+    "password": "1234",
+    "database": "practice"
+}
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection(conn
+//   {
+//   host: conf.host,
+//   user:conf.user,
+//   password:conf.password,
+//   port:conf.port,
+//   database:conf.database
+// }
+);
+
+connection.connect();
+
 app.get('/api/customers',(req,res)=>{
-    res.send([{
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '김예림',
-        'birthday': '060118',
-        'gender': '여자',
-        'job': 'student'
-      }, {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '김예봄',
-        'birthday': '060118',
-        'gender': '여자',
-        'job': 'student'
-      }, {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '김주완',
-        'birthday': '060118',
-        'gender': '남자',
-        'job': 'student'
-      }]
-      );
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err,rows,fields)=>{
+        res.send(rows);
+      }
+    )
 
 })
 app.listen(port,()=>
